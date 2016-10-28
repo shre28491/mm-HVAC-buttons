cf94b50731814f56d6ce52ef2a1a7e1cdec8aca0
shre28491 <shre28491@gmail.com>
2016-10-28 07:26:05 -0700

    Add files via upload

---
+++ mm-HVAC-buttons.js
@@ -0,0 +1,60 @@
+/* Magic Mirror
+ * Module: HVAC Buttons
+ * By Shreyas
+ * 
+ */
+
+Module.register("mm-HVAC-buttons",{
+        //Default module config
+    defaults:{
+        fadeDuration: 2000
+    },
+    getScripts: function(){
+        return ["jquery.js" , "jquery-ui.min.js"];
+    },
+    getStyles: function(){
+        return ["styles.css"];
+    },
+    // Define a start method. For example if you want to schedule timers,
+    // or want to make connections to socket servers. this is optional
+    start: function(){
+        Log.info('Starting module:'+ this.name);
+    },
+    //Overide DOM generator
+    getDom: function() {
+        var wrapper = document.createElement("div");
+        var buttonElement = document.createElement("BUTTON");
+        var HVACwrapper = document.createElement("div");
+        var ventselSlider = document.createElement("div");
+        var blspeedDial = document.createElement("div");
+        var regenToggle = document.createElement("a");
+        var regenDial = document.createElement("div");
+        
+	wrapper.className = "wrap";      
+        HVACwrapper.className = "hvac";
+	ventselSlider.className = "ventsel";
+        blspeedDial.className = "blspeed";
+        regenToggle.className = "regen";
+        
+                
+        wrapper.appendChild(HVACwrapper);
+        HVACwrapper.appendChild(ventselSlider);
+        HVACwrapper.appendChild(blspeedDial);
+        HVACwrapper.appendChild(regenToggle);
+        HVACwrapper.appendChild(regenDial);
+	
+        
+        var fadeDuration = this.config.fadeDuration;
+        $(document).ready(function(){
+                $("a").click(function(){
+			debugger;
+	    		Log.info('mouse-click-event');
+            		$(this).toggleClass('off');
+        	});
+	});
+        return wrapper;
+    }
+});
+        
+            
+        
